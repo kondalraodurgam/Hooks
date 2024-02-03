4c65f1dc3343b9f122e8a3bdfd05c3fa40380c0c
Kondal Durgam <kondaldurgam733@gmail.com>
2024-02-03 17:59:37 +0530

    useEffect API example

---
+++ UseEffectAPI.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+function UseEffectAPI(){
+    const [data,setData] = useState([]);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((result)=>result.json())
+        .then((data)=>{
+            console.log(data)
+            setData(data)
+        }).catch((err)=>{
+            console.log('Error fetching user data:',err);
+        })
+    }, []);
+    return (
+        <div>
+            <h2>
+                useEffect API Hook example
+            </h2>
+            <ul>
+                {data.map((dat)=>(
+                    <li key={dat.id}>{dat.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+export default UseEffectAPI;
