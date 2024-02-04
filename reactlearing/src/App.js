ecf17991d06ccf32a03aabbcac86f942558e8dd3
Kondal Durgam <kondaldurgam733@gmail.com>
2024-02-04 04:30:49 +0000

    Updated Files

---
+++ reactlearing/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+// import UseState from './components/UseState';
+// import UseEffect from './components/UseEffect';
+// import UseEffectAPI from './components/UseEffectAPI';
+import UseContext from "./components/UseContext";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UseState /> */}
+      {/* <UseEffect /> */}
+      {/* <UseEffectAPI /> */}
+      <UseContext />
+    </div>
+  );
+}
+
+export default App;
