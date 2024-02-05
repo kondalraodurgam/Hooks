b024f8ba94a297c367274ef0267c9ed50013efdc
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2024-02-05 06:29:27 +0000

    p2

--- reactlearing/src/components/UseState.js
+++ reactlearing/src/components/UseState.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function UseState() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [output, setOutput] = useState("");
 
   function onChangeFirstName(e) {
     setFirstName(e.target.value);
@@ -10,16 +11,23 @@ function UseState() {
   function onChangeLastName(e) {
     setLastName(e.target.value);
   }
+  function onSave(e) {
+    e.preventDefault();
+    setOutput(firstName + " " + lastName + " saved!");
+  }
 
   return (
     <div>
-      <h2>useState hook example</h2>
-      <br />
+      <h2>useState hook example</h2> <br />
       First name:
       <input type="text" value={firstName} onChange={onChangeFirstName} />
       <br />
       Last name:
-      <input type="text" value={lastName} onChange={onChangeLastName} />
+      <input type="text" value={lastName} onChange={onChangeLastName} /> <br />
+      <hr />
+      <input type="button" onClick={onSave} value="Save" /> <br />
+      <div>{output}</div>
+      <br />
     </div>
   );
 }
